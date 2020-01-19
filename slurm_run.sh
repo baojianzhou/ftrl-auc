00296a798d43f8904ac2d338055dd82896dac1b7
Baojian Zhou <bzhou6@albany.edu>
2020-01-19 05:03:17 -0500

    dd

--- slurm_run.sh
+++ slurm_run.sh
@@ -6,6 +6,7 @@
 #SBATCH --ntasks=1
 #SBATCH --time=96:00:00
 #SBATCH --cpus-per-task=27
-#SBATCH --output=/network/rit/lab/ceashpc/bz383376/git/ftrl-auc/logs/re_webspam.out
+#SBATCH --output=/network/rit/lab/ceashpc/bz383376/git/ftrl-auc/logs/re_rcv1_bin.out
 # /network/rit/lab/ceashpc/bz383376/opt/env-python2.7.14/bin/python data_preprocess.py
 /network/rit/lab/ceashpc/bz383376/opt/env-python2.7.14/bin/python test_on_high_dim.py
+# /network/rit/lab/ceashpc/bz383376/opt/env-python2.7.14/bin/python data_preprocess.py
