7165a7fb3bd2afa5b96c50f939f1df29d58053dc
Baojian Zhou <bzhou6@albany.edu>
2020-01-20 05:25:39 -0500

    dd

--- slurm_run.sh
+++ slurm_run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=test
-#SBATCH --nodelist=ceashpc-09
+#SBATCH --job-name=solam
+#SBATCH --nodelist=ceashpc-07
 #SBATCH --partition=ceashpc
 #SBATCH --mem=100G
 #SBATCH --ntasks=1
 #SBATCH --time=96:00:00
 #SBATCH --cpus-per-task=10
-#SBATCH --output=/network/rit/lab/ceashpc/bz383376/git/ftrl-auc/logs/re_03_real_sim_fsauc.out
-/network/rit/lab/ceashpc/bz383376/opt/env-python2.7.14/bin/python test_on_high_dim.py run fsauc 03_real_sim 10
+#SBATCH --output=/network/rit/lab/ceashpc/bz383376/git/ftrl-auc/logs/re_05_rcv1b_solam.out
+/network/rit/lab/ceashpc/bz383376/opt/env-python2.7.14/bin/python test_on_high_dim.py run solam 05_rcv1_bin 10
