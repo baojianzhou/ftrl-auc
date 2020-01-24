6dcf5250f13b592e66ac32f42523e32a1eed9c6d
Baojian Zhou <bzhou6@albany.edu>
2020-01-24 05:47:16 -0500

    dd

--- slurm_run_huge.sh
+++ slurm_run_huge.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=ftrl-fast
-#SBATCH --nodelist=ceashpc-10
+#SBATCH --nodelist=ceashpc-07
 #SBATCH --partition=ceashpc
-#SBATCH --mem=25G
+#SBATCH --mem=100G
 #SBATCH --ntasks=1
-#SBATCH --array=00-09
+#SBATCH --array=00-00
 #SBATCH --time=96:00:00
 #SBATCH --cpus-per-task=1
-#SBATCH --output=/network/rit/lab/ceashpc/bz383376/git/ftrl-auc/logs/re_07_url_ftrl_fast_%A_%02a.out
-/network/rit/lab/ceashpc/bz383376/opt/env-python2.7.14/bin/python test_on_high_dim.py run_huge ftrl_fast 07_url $SLURM_ARRAY_TASK_ID
+#SBATCH --output=/network/rit/lab/ceashpc/bz383376/git/ftrl-auc/logs/re_04_avazu_ftrl_fast_%A_%02a.out
+/network/rit/lab/ceashpc/bz383376/opt/env-python2.7.14/bin/python test_on_high_dim.py run_huge ftrl_fast 04_avazu $SLURM_ARRAY_TASK_ID
