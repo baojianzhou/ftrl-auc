6a70badd342575d779a7ae4199fda3e70cdc7560
Baojian Zhou <bzhou6@albany.edu>
2020-02-07 07:21:06 -0500

    dd

--- data_preprocess.py
+++ data_preprocess.py
@@ -13,97 +13,6 @@ from sklearn.model_selection import KFold
 
 root_path = '/network/rit/lab/ceashpc/bz383376/data/kdd20/'
 
-
-def data_process_00_sentiment(data_name='00_sentiment'):
-    np.random.seed(int(time.time()))
-    data_path = '/network/rit/lab/ceashpc/bz383376/data/kdd20/00_sentiment/processed_acl/books/'
-    data = dict()
-    # sparse data to make it linear
-    data['x_tr_vals'] = []
-    data['x_tr_inds'] = []
-    data['x_tr_poss'] = []
-    data['x_tr_lens'] = []
-    data['y_tr'] = []
-    data['words'] = dict()
-    data['total_words'] = 0
-    prev_posi, min_id, max_id, max_len = 0, np.inf, 0, 0
-    with open(data_path + 'positive.review') as f:
-        for index, each_line in enumerate(f.readlines()):
-            items = each_line.lstrip().rstrip().split(' ')
-            data['y_tr'].append(1.0)
-            cur_values, cur_indices = [], []
-            for _ in items[:-1]:
-                word, count = _.split(':')[0], float(_.split(':')[1])
-                if word not in data['words']:
-                    data['words'][word] = data['total_words']
-                    cur_values.append(count)
-                    cur_indices.append(data['total_words'])
-                    data['total_words'] += 1
-                else:
-                    cur_values.append(count)
-                    cur_indices.append(data['words'][word])
-            xx = np.asarray(cur_values)
-
-            data['x_tr_vals'].extend(list(xx / np.linalg.norm(xx)))
-            data['x_tr_inds'].extend(cur_indices)
-            data['x_tr_poss'].append(prev_posi)
-            data['x_tr_lens'].append(len(cur_indices))
-            prev_posi += len(cur_indices)
-            if len(cur_indices) != 0:
-                min_id = min(min(cur_indices), min_id)
-                max_id = max(max(cur_indices), max_id)
-                max_len = max(len(cur_indices), max_len)
-            else:
-                print('warning, all features are zeros! of %d' % index)
-
-    with open(data_path + 'negative.review') as f:
-        for index, each_line in enumerate(f.readlines()):
-            items = each_line.lstrip().rstrip().split(' ')
-            data['y_tr'].append(-1.0)
-            cur_values, cur_indices = [], []
-            for _ in items[:-1]:
-                word, count = _.split(':')[0], float(_.split(':')[1])
-                if word not in data['words']:
-                    data['words'][word] = data['total_words']
-                    cur_values.append(count)
-                    cur_indices.append(data['total_words'])
-                    data['total_words'] += 1
-                else:
-                    cur_values.append(count)
-                    cur_indices.append(data['words'][word])
-            xx = np.asarray(cur_values)
-            data['x_tr_vals'].extend(list(xx / np.linalg.norm(xx)))
-            data['x_tr_inds'].extend(cur_indices)
-            data['x_tr_poss'].append(prev_posi)
-            data['x_tr_lens'].append(len(cur_indices))
-            prev_posi += len(cur_indices)
-            if len(cur_indices) != 0:
-                min_id = min(min(cur_indices), min_id)
-                max_id = max(max(cur_indices), max_id)
-                max_len = max(len(cur_indices), max_len)
-            else:
-                print('warning, all features are zeros! of %d' % index)
-    print(min_id, max_id, max_len)
-    data['x_tr_vals'] = np.asarray(data['x_tr_vals'], dtype=float)
-    data['x_tr_inds'] = np.asarray(data['x_tr_inds'], dtype=np.int32)
-    data['x_tr_lens'] = np.asarray(data['x_tr_lens'], dtype=np.int32)
-    data['x_tr_poss'] = np.asarray(data['x_tr_poss'], dtype=np.int32)
-    data['y_tr'] = np.asarray(data['y_tr'], dtype=float)
-    data['n'] = len(data['y_tr'])
-    data['p'] = 195887
-    assert len(np.unique(data['y_tr'])) == 2  # we have total 2 classes.
-    print('number of positive: %d' % len([_ for _ in data['y_tr'] if _ > 0]))
-    print('number of negative: %d' % len([_ for _ in data['y_tr'] if _ < 0]))
-    data['num_posi'] = len([_ for _ in data['y_tr'] if _ > 0])
-    data['num_nega'] = len([_ for _ in data['y_tr'] if _ < 0])
-    data['num_nonzeros'] = len(data['x_tr_vals'])
-    data['name'] = '00_sentiment'
-    for run_id in range(10):
-        rand_perm = np.random.permutation(data['n'])
-        data['rand_perm_%d' % run_id] = rand_perm
-    pkl.dump(data, open(os.path.join(data_path, 'data_00_sentiment.pkl'), 'wb'))
-
-
 def data_process_01_webspam_small():
     data_path = '/network/rit/lab/ceashpc/bz383376/data/kdd20/01_webspam/'
     data = dict()
@@ -634,6 +543,7 @@ def data_process_06_pcmac(num_trials=10):
         all_indices = np.random.permutation(data['n'])
         data['trial_%d_all_indices' % _] = np.asarray(all_indices, dtype=np.int32)
         assert data['n'] == len(data['trial_%d_all_indices' % _])
+        print(all_indices[:5])
         tr_indices = all_indices[:int(len(all_indices) * 4. / 6.)]
         data['trial_%d_tr_indices' % _] = np.asarray(tr_indices, dtype=np.int32)
         va_indices = all_indices[int(len(all_indices) * 4. / 6.):int(len(all_indices) * 5. / 6.)]
@@ -644,7 +554,7 @@ def data_process_06_pcmac(num_trials=10):
         n_va = len(data['trial_%d_va_indices' % _])
         n_te = len(data['trial_%d_te_indices' % _])
         assert data['n'] == (n_tr + n_va + n_te)
-    pkl.dump(data, open(os.path.join(root_path, '06_pcmac/processed_06_pcmac.pkl'), 'wb'))
+    return data
 
 
 def data_process_07_url(num_trials=10):
@@ -1037,7 +947,7 @@ def data_process_11_reviews(num_trials=10):
 
 
 def main():
-    data_process_11_reviews()
+    data_process_06_pcmac()
 
 
 if __name__ == '__main__':
